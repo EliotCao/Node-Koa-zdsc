4575c0e072e51c0e237fedeb7386c1ad4e643b4f
EliotCao <484900039@qq.com>
2022-05-08 19:20:38 +0800

    Add JWT

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,8 @@
-const {createUser} = require('../service/user.service')
+const jwt = require('jsonwebtoken');
+
+const {createUser, getUserInfo} = require('../service/user.service')
 const {userRegisterError} = require('../constant/err.type')
+const {JWT_SECRET} = require('../config/config.default')
 
 class UserController{
     async register(ctx, next){
@@ -28,7 +31,20 @@ class UserController{
     //Login
     async login(ctx, next) {
         const {username} = ctx.request.body
-        ctx.body = 'login success'
+
+        //1.获取用户信息(在token的playload中，记录id,username,is_admin)
+        try {
+            const {password, ...res} = await getUserInfo({username})
+            ctx.body = {
+                code: 0,
+                message: '用户登录成功',
+                result: {
+                    token: jwt.sign(res, JWT_SECRET, {expiresIn: '1d'})
+                }
+            }
+        }catch (err) {
+            console.error('用户登录失败', err)
+        }
     }
 }
 
