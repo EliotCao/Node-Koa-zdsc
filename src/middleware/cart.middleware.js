e99f2dd14b56b8ddb46093ff464b6c317c3f4125
EliotCao <484900039@qq.com>
2022-05-14 11:26:02 +0800

    Fix cart middleware

--- src/middleware/cart.middleware.js
+++ src/middleware/cart.middleware.js
@@ -12,20 +12,6 @@ const validator = (rules) => {
     }
 }
 
-/*    async (ctx, next) => {
-    try {
-        ctx.verifyParams({
-            goods_id: 'number',
-        })
-    }catch (err){
-        console.error(err)
-        invalidGoodsID.result = err
-        return ctx.app.emit('error',invalidGoodsID, ctx)
-    }
-
-    await next()
-}*/
-
 module.exports = {
     validator
 }
