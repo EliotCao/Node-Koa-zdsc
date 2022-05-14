2a803cd0c858ba862add2a1b1a9daf513df62ac0
EliotCao <484900039@qq.com>
2022-05-14 18:04:02 +0800

    Add findAllAddr

--- src/router/addr.route.js
+++ src/router/addr.route.js
@@ -2,7 +2,7 @@ const Router = require('koa-router')
 
 const {auth} = require('../middleware/auth.middleware')
 const {validator} = require('../middleware/addr.middleware')
-const {create} = require('../controller/addr.controller')
+const {create, findAll} = require('../controller/addr.controller')
 
 const router = new Router({prefix: '/address'})
 
@@ -11,5 +11,8 @@ router.post('/',auth, validator({
     phone: {type: 'string', format: /^1\d{10}$/},
     address: 'string'
 }), create)
+
+// 获取地址列表
+router.get('/', auth, findAll)
 
 module.export = router
