bc7f78301a06f7f26c0d112d23c5b561b799bc6b
EliotCao <484900039@qq.com>
2022-05-14 18:02:53 +0800

    Add findAllAddr

--- src/controller/addr.controller.js
+++ src/controller/addr.controller.js
@@ -1,4 +1,4 @@
-const {createAddr} = require('../service/addr.service')
+const {createAddr, findAllAddr} = require('../service/addr.service')
 
 class AddrController {
     async create(ctx) {
@@ -10,6 +10,18 @@ class AddrController {
         ctx.body = {
             code: 0,
             message: '添加地址成功',
+            result: res
+        }
+    }
+
+    async findAll(ctx) {
+        //解析user_id, consignee, phone, address
+        const user_id = ctx.state.user.id
+
+        const res = await findAllAddr(user_id)
+        ctx.body = {
+            code: 0,
+            message: '获取地址列表成功',
             result: res
         }
     }
--- src/service/addr.service.js
+++ src/service/addr.service.js
@@ -4,6 +4,15 @@ class AddrService {
     async createAddr(addr) {
         return await Address.creat(addr)
     }
+
+    async findAllAddr(user_id) {
+        return await Address.findAll({
+            attributes: ['id', 'consignee', "consignee", 'phone', 'address', 'is_default'],
+            where: {
+                user_id
+            }
+        })
+    }
 }
 
 module.exports = new AddrService()
