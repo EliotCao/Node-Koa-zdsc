152e675f6a2e791880e2d1848a275769aa7f1f68
EliotCao <484900039@qq.com>
2022-05-15 15:13:18 +0800

    Add order route

---
+++ src/router/order.route.js
@@ -0,0 +1,16 @@
+const Router = require('koa-router')
+
+const {auth} = require('../middleware/auth.middleware')
+const {validator} = require('../middleware/order.middleware')
+const {create} = require('../controller/order.controller')
+
+const router = new Router({prefix: '/orders'})
+
+//提交订单
+router.post('/',auth,validator({
+    address_id: 'int',
+    goods_info: 'string',
+    total: 'string'
+}), create)
+
+module.exports = router
