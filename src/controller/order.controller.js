9406f1dc3259ca86e056bd0e40adc5434bb10b4c
EliotCao <484900039@qq.com>
2022-05-15 15:10:09 +0800

    Add order controller

---
+++ src/controller/order.controller.js
@@ -0,0 +1,28 @@
+const {createOrder} = require('../service/order.service')
+
+class OrderController {
+    async create(ctx) {
+        //准备
+        const user_id = ctx.state.user.id
+        const {address_id, goods_info, total} = ctx.request.body
+
+        const order_number = 'XZD' + Date.now()
+
+        const res = await createOrder({
+            user_id,
+            address_id,
+            goods_info,
+            total,
+            order_number
+        })
+
+        ctx.body = {
+            code: 0,
+            message: '生成订单',
+            result: res
+        }
+    }
+
+}
+
+module.exports = new OrderController()
